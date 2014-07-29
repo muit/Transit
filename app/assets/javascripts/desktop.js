3e6be713ddf4644af77ee5dda69adc914f72714f
muit <miguel_3c@hotmail.com>
2014-07-29 16:37:54 +0200

    Desktop(TukTuk) / Mobile(Lungo) setup

--- app/assets/javascripts/desktop.js
+++ app/assets/javascripts/desktop.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
-//= require_tree .
+//= require_tree ./desktop
+
 $( document ).ready(function() {
     if(window.google)
         MapSystem.loadMap();
---
+++ app/assets/javascripts/mobile.js
@@ -0,0 +1,62 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require jquery_ujs
+//= require ./mobile/quo
+//= require ./mobile/lungo
+
+var Util = {
+    createNotification: function (img, title, content){
+
+        if (Notification.permission == "granted") {
+            var n = new Notification(title, {icon: img, body: content});
+            setTimeout(function(){
+                n.close();
+            },5000);
+        }
+        else{
+            Notification.requestPermission(function(){
+                if(Notification.permission == "granted"){
+                    var n = new Notification(title, {icon: img, body: content});
+                    setTimeout(function(){
+                       n.close();
+                    },5000);
+                }
+            });
+        }
+    },
+
+    setText: function(name, text){
+        localStorage.setItem(name, text);
+    },
+
+    getText: function(name){
+        var a = localStorage.getItem(name);
+        if(a == null)
+            return "";
+        return a;
+    },
+
+    getId: function(id){
+        return document.getElementById(id);
+    },
+
+    Trigger: function(){
+        this.state = true;
+        this.get = function(){
+            var rstate = this.state;
+            this.state = false;
+            return rstate;
+        }
+    }
+}
