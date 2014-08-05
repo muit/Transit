f9295a3609f94d0fac0065879eaf57259b32e890
muit <miguel_3c@hotmail.com>
2014-08-05 17:43:37 +0200

    [Update] Search Stop times Algorithm

--- app/controllers/stoptime_controller.rb
+++ app/controllers/stoptime_controller.rb
@@ -1,2 +1,7 @@
 class StoptimeController < ApplicationController
+  def index
+    station_id = params[:station_id]
+    stops = StopTime.where(station_id: station_id).where(arrival: params[:from]..params[:to])
+    render :json => stops
+  end
 end
